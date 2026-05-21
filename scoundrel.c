0de1dace9928d2307766509955ab31162f016be4
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-05-21 06:12:24 -0300

    0.2

--- scoundrel.c
+++ scoundrel.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main() {
     struct carta {
         int numero, naipe;
     };
 
-    struct carta cartas[44];
-    int indice_carta = 0, cont = 15;
+    struct carta cartas[44], cartas_emb[88], cartas_atuais[4];
+    struct carta arma;
+    arma.numero = 0;
+
+    int indice_carta = 0, cont = 15, game = 0, vida = 20, pos_cartas = 0, indice = 0, fim_bar = 44, pulou = 0, escolha1, escolha2, indices_emb[44];
+
+    char nome_naipe[10];
 
     //========================= PREENCHENDO A MATRIZ DE CARTAS COM OS NÚMEROS E NAIPES =========================
 
@@ -32,12 +38,6 @@ int main() {
     printf("  ___) | (_| (_) | |_| | | | | (_| | | |  __/ |\n");
     printf(" |____/ \\___\\___/ \\__,_|_| |_|\\__,_|_|  \\___|_|\n");
     printf("=====================================================\n");
-    printf("     Digite qualquer coisa para iniciar!\n");
-    printf("=====================================================\n");
-    scanf(" ");
-
-    struct carta cartas_emb[44];
-    int indices_emb[44];
 
     for (int i = 0; i < 44; i++) {
         indices_emb[i] = i;
@@ -47,6 +47,100 @@ int main() {
 
     for (int i = 0; i < 44; i++) {
         cartas_emb[indices_emb[i]] = cartas[i];
+    }
+
+    while (game == 0) {
+        for (int i = pos_cartas; i < pos_cartas+4; i++) {
+            cartas_atuais[indice] = cartas_emb[i];
+
+            indice++;
+            
+            if (cartas_emb[i].naipe == 0) {
+                strcpy(nome_naipe, "Paus");
+            } else if (cartas_emb[i].naipe == 1) {
+                strcpy(nome_naipe, "Espadas");
+            } else if (cartas_emb[i].naipe == 2) {
+                strcpy(nome_naipe, "Ouros");
+            } else if (cartas_emb[i].naipe == 3) {
+                strcpy(nome_naipe, "Copas");
+            }
+
+            printf("\n-------------------\n");
+            printf("[%d]  %d   %s", indice, cartas_emb[i].numero, nome_naipe);
+            printf("\n-------------------\n");
+        }
+
+        pos_cartas += 4; // Avançando no baralho. O array de cartas atuais agora irá conter 4 cartas a frente das que ele continha
+
+        printf("\nVida: %d || Arma: %d\n", vida, arma.numero);
+        printf("Digite 1 para permanecer na sala ou 2 para pular: ");
+        scanf("%d", &escolha1);
+
+        if (escolha1 == 1) {
+            printf("Escolha com quais cartas interagir: ");
+            
+            for (int i = 0; i < 3; i++) {
+                scanf("%d", &escolha2);
+
+                escolha2--;
+
+                if (escolha2 > 3 || escolha2 < 0) {
+                    printf("Escolha um valor valido.");
+                    return 1;
+                }
+
+                if (cartas_atuais[escolha2].naipe == 0) {
+                    if (arma.numero == 0) {
+                        vida -= cartas_atuais[escolha2].numero;
+                    } else if (arma.numero != 0) {
+                        if (cartas_atuais[escolha2].numero - arma.numero >= 0) {
+                            vida -= cartas_atuais[escolha2].numero - arma.numero;
+                        } else {
+                            vida = vida;
+                        }
+                    }
+                } else if (cartas_atuais[escolha2].naipe == 1) {
+                    if (arma.numero == 0) {
+                        vida -= cartas_atuais[escolha2].numero;
+                    } else if (arma.numero != 0) {
+                        vida -= cartas_atuais[escolha2].numero - arma.numero;
+                    }
+                } else if (cartas_atuais[escolha2].naipe == 2) {
+                    arma = cartas_atuais[escolha2];
+                } else if (cartas_atuais[escolha2].naipe == 3) {
+                    vida += cartas_atuais[escolha2].numero;
+                }
+
+                if (vida < 1) {
+                    printf("Game over :(\n");
+                    printf("Pontuação: hdjtfgbdhfghgds");
+                }
+
+                printf("\nVida: %d || Arma: %d\n", vida, arma.numero);
+            }
+
+            pulou = 0;
+        } else if (escolha1 == 2 && pulou == 0) {
+            indice = 0;
+
+            for (int i = fim_bar; i < fim_bar+4; i++) {
+                cartas_emb[i] = cartas_atuais[indice];
+                indice++;
+            }
+
+            fim_bar += 4;
+            pulou = 1;
+        } else  if (escolha1 > 2) {
+            printf("\n-----------------------------------------------------------------\n");
+            printf("Escolha um valor valido.");
+            return 1;
+        } else if (pulou == 1) {
+            printf("\n-----------------------------------------------------------------\n");
+            printf("Nao pode pular essa sala pois a anterior ja foi pulada!");
+            return 1;
+        }
+
+        indice = 0;
     }
 
     return 0;
