eda7e75daac6579293172fb22f78c030a71deaa2
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-06-05 23:52:25 -0300

    Update scoundrel_auto.c

--- scoundrel_auto.c
+++ scoundrel_auto.c
@@ -1,2 +1,3 @@
 // Atenção que agora a gente vai ter que usar funções
-//Seria bom reaproveitar o código antigo
+// Seria bom reaproveitar o código antigo
+// Tem que consertar os problemas no seu código: Tem que mostrar o que acontece em cada turno, tem que mostrar o limite da arma, tem que consertar o fim do baralho
