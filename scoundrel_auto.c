6b208804965bfa11fdd7599cae1211e0eaa9866f
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-06-10 03:24:14 -0300

    Update scoundrel_auto.c

--- scoundrel_auto.c
+++ scoundrel_auto.c
@@ -7,5 +7,3 @@
 // Tem que consertar os problemas no seu código: Tem que mostrar o que acontece em cada turno, tem que mostrar o limite da arma, tem que consertar o fim do baralho
 // A Re e o Caio vão ter que fazer um beta test mais rigoroso
 // TEM QUE COLOCAR O NOME DOS PARTICIPANTES
-
-owieejfosdofsdhfsudhfjsd
